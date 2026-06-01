268ace98036153dd248b81a120796432c49b2d05
Kunal Choudhary <kc740445@gmail.com>
2026-06-01 18:35:49 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+
+const app = express();
+const server  = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () =>{
+    const connectionDB = await mongoose.connect("mongodb+srv://Apnavideocall:Apnavideocall@apnavideocall.quk304w.mongodb.net/?appName=Apnavideocall")
+    console.log(`MONGO Connected DB Host ${connectionDB.connection.host}`)
+    server.listen(app.get("port"), () =>{
+        console.log("LISTENING ON PORT 8000");
+    });
+}
+
+start();
